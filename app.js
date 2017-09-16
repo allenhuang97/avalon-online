2a97bddcbacaa4bd846a1672d25fd40696a1263a
Melinda Jing <z3jing@edu.uwaterloo.ca>
2017-09-16 10:04:31 -0400

    add middleware

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var server = app.listen(3000, function () {
     console.log('app listening on port 3000!')
 });
 var io = require('socket.io').listen(server);
-
+app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
       res.sendfile(__dirname + '/index.html');
 });
