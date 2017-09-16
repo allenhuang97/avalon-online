78bfbac340f3fa3227b2ad6f0999f3f7246bda10
Melinda Jing <z3jing@edu.uwaterloo.ca>
2017-09-15 22:20:35 -0400

    basic node sever set up

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var server = app.listen(3000, function () {
+    console.log('app listening on port 3000!')
+});
+var io = require('socket.io').listen(server);
+
+app.get('/', function (req, res) {
+      res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+      socket.emit('news', { hello: 'world' });
+        socket.on('my other event', function (data) {
+                console.log(data);
+                  });
+});
